f74e132253f37135a86d6065c0ffd78886be5355
Githmaz <Githmaz1@gmail.com>
2025-03-08 10:35:49 +0530

    feat: add main structure for all 4 team member cards

---
+++ js/team.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".team-member").forEach(member => {
+    member.addEventListener("mouseover", () => {
+        member.classList.add("expanded");
+    });
+
+    member.addEventListener("mouseout", () => {
+        member.classList.remove("expanded");
+    });
+});

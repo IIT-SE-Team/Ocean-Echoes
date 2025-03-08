9a75a1a2713bf357a4bfe32afa7d9dcbac7fd9f1
ushalkakapugamage <ushalkakapugamage@gmail.com>
2025-03-08 21:35:24 +0530

    added new global styles, modified nav bar styles

--- js/navbar.js
+++ js/navbar.js
@@ -1,49 +1,17 @@
 // navbar.js
 
-function calculateSettingAsThemeString({ localStorageTheme }) {
-  if (localStorageTheme !== null) {
-    return localStorageTheme;
-  }
-  
-  return "light";
-}
-
-const localStorageTheme = localStorage.getItem("theme");
-
-const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-
-let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme});
-
-const button = document.querySelector("[data-theme-toggle]");
-
-button.addEventListener("click", () => {
-  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-
-  console.log({newTheme});
-  
-  button.innerText = newTheme;
-
-
-  button.setAttribute("aria-label", newTheme);
-
-  document.querySelector("html").setAttribute("data-theme", newTheme);
-  
-  localStorage.setItem("theme", newTheme);
-  
-  currentThemeSetting = newTheme;
-});
 
 window.addEventListener('scroll', () => {
-  const navbar = document.getElementById('navbar');
+  const header = document.getElementById('header');
   const navLinks = document.querySelectorAll('#navbar a');
   
   if (window.scrollY > 700) {
-    navbar.style.background = ''; 
+    header.style.background = '#b3d6e9'; 
     navLinks.forEach(link => {
       link.style.color = ''; 
     });
   } else {
-    navbar.style.background = 'transparent'; 
+    header.style.background = 'transparent'; 
     navLinks.forEach(link => {
       link.style.color = '#FBFFF4'; 
     });
@@ -57,39 +25,6 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 
-const openButton = document.getElementById('open-sidebar-button')
-const navbar = document.getElementById('navbar')
-
-const media = window.matchMedia("(width < 700px)")
-
-media.addEventListener('change', (e) => updateNavbar(e))
-
-function updateNavbar(e) {
-    const isMobile = e.matches
-    console.log(isMobile)
-    if (isMobile) {
-        navbar.setAttribute('inert', '')
-    }
-    else {
-        // desktop device
-        navbar.removeAttribute('inert')
-    }
-}
-
-function openSidebar() {
-    navbar.classList.add('show')
-    openButton.setAttribute('aria-expanded', 'true')
-    navbar.removeAttribute('inert')
-}
-
-function closeSidebar() {
-    navbar.classList.remove('show')
-    openButton.setAttribute('aria-expanded', 'false')
-    navbar.setAttribute('inert', '')
-}
-
-
-updateNavbar(media)
 
 document.addEventListener("DOMContentLoaded", function () {
     const navLinks = document.querySelectorAll("#navbar ul li a");

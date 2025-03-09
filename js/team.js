540160c82d61202c8d15e8085ec0a98f892cb900
Githmaz <Githmaz1@gmail.com>
2025-03-10 02:23:18 +0530

    feat: complete team page with footer and keyboard navigation support

--- js/team.js
+++ js/team.js
@@ -1,12 +1,11 @@
 document.querySelectorAll(".team-member").forEach(member => {
     member.addEventListener("mouseover", () => {
         member.classList.add("expanded");
+        member.setAttribute("aria-expanded", "true");
     });
 
     member.addEventListener("mouseout", () => {
         member.classList.remove("expanded");
+        member.setAttribute("aria-expanded", "false");
     });
-
-
 });
-
